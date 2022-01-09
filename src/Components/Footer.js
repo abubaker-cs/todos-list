40695b9802ffde9fd4466f95b653996543e064d6
abubaker <mohammad_abubaker@yahoo.com>
2022-01-10 02:21:34 +0500

    Implemented Styles and taking break

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,19 @@
 import React from "react";
+import "./footer.css";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+
+  let footerStyle = {
+    position: "absolute",
+    top: "100vh",
+    width: "100%"
+  }
+
+  return (
+    <footer className="bg-dark text-light py-3" style={footerStyle}>
+      <p className="text-center">
+        Copyright &copy; 2022 <a href="https://abubaker.org/">ACS</a>
+      </p>
+    </footer>
+  );
 };

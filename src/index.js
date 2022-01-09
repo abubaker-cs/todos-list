153a6d2b7db802739e37d71fd8584b5a247b8f6a
abubaker <mohammad_abubaker@yahoo.com>
2022-01-09 18:31:37 +0500

    Adding navbar from bootstrap

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
+  // JSX
   <React.StrictMode>
-    <App />
+    <App /> {/* App.js */}
   </React.StrictMode>,
-  document.getElementById('root')
+
+  // Target ID in the index.html
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
